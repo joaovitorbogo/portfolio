5720e8db395f370ac1a48cb076e38ea8939c8b33
João Vitor Ferreira Bogo <joaovitor_ferreirabogo@hotmail.com>
2023-08-07 23:47:20 -0300

    Adicionando arquivos iniciais

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 # Portifolio
 Meu portifólio, mostrando um pouco do que eu sou capaz.
 
-Aos poucos irei adicionando o conteúdo."# portfolio" 
-"# portfolio" 
-"# portfolio" 
+Aos poucos irei adicionando o conteúdo.
---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Meu Portfólio</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <li><a href="#hero">Início</a></li>
+                <li><a href="#portfolio">Portfólio</a></li>
+                <li><a href="#about">Sobre Mim</a></li>
+                <li><a href="#contact">Contato</a></li>
+            </ul>
+        </nav>
+    </header>
+
+    <section id="hero">
+        <h1>Bem-vindo ao meu portfólio</h1>
+        <p>Veja meus projetos e habilidades.</p>
+    </section>
+
+    <section id="portfolio">
+        <h2>Portfólio</h2>
+        <p>Aqui estão alguns dos meus projetos:</p>
+        <!-- Listagem de projetos -->
+    </section>
+
+    <section id="about">
+        <h2>Sobre Mim</h2>
+        <p>Um pouco sobre mim e minha experiência:</p>
+        <!-- Conteúdo sobre você -->
+    </section>
+
+    <section id="contact">
+        <h2>Contato</h2>
+        <p>Entre em contato comigo:</p>
+        <!-- Formulário de contato ou informações de contato -->
+    </section>
+
+    <footer>
+        <p>&copy; 2023 Meu Portfólio.</p>
+    </footer>
+</body>
+</html>
